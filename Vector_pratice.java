d6316e6ba553ec44f2daee6bd03f57d232967cdd
ambler082 <zzzodg@gmail.com>
2020-09-21 19:42:25 +0900

    Vector_1

---
+++ Vector_pratice.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Vector_pratice {
+
+	public static void main(String[] args) {
+		int size;
+		List<String> list = new Vector<String>();
+		
+		list.add("A");
+		list.add("B");
+		list.add("C");
+
+	}
+
+}
