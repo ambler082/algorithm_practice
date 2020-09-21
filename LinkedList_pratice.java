9ef0f562b017cb0db4b3cb098c6280922d761548
ambler082 <zzzodg@gmail.com>
2020-09-21 23:55:37 +0900

    LinkedList_pratice

---
+++ LinkedList_pratice.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class LinkedList_pratice {
+
+	public static void main(String[] args) {
+		LinkedList list = new LinkedList();
+		
+		list.add(10);
+		list.addLast(20);
+		list.addFirst(30);
+			
+		System.out.println(list);
+	}
+}
