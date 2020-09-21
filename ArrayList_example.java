f1cd3def525e0668e555659cf5eced57c82dc94c
ambler082 <zzzodg@gmail.com>
2020-09-21 18:58:59 +0900

    ArrayList_1

---
+++ ArrayList_example.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class ArrayList_example {
+
+	public static void main(String[] args) {
+		int size;
+		List<String> list = new ArrayList<String>();
+		
+		list.add("A");
+		list.add("B");
+		list.add("C");
+
+	}
+
+}
