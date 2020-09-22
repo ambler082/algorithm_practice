cfb892be5d3868a12f6439f4cb1d8c9b21d65874
ambler082 <zzzodg@gmail.com>
2020-09-23 04:20:17 +0900

    ArrayList_1

---
+++ Arraylist_pratice.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Arraylist_pratice {
+
+	public static void main(String[] args) {
+		ArrayList list = new ArrayList<String>();
+
+		list.add("A");
+		list.add("B");
+		list.add("C");
+		
+		System.out.print(list);
+	}
+
+}
