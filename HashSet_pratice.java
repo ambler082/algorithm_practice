55ac670ef9d4fea3c0489a4d13b03c80f2baf74d
ambler082 <zzzodg@gmail.com>
2020-09-23 10:23:30 +0900

    HashSet

---
+++ HashSet_pratice.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class HashSet_pratice {
+
+	public static void main(String[] args) {
+		HashSet set = new HashSet();
+		
+		set.add(1);
+		
+		System.out.println(set);
+	}
+
+}
