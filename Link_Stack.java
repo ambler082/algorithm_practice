3112a481e6c115a1bc4fb8dd30aa0cf4a35199ef
ambler082 <zzzodg@gmail.com>
2020-09-27 15:21:32 +0900

    pop

--- Link_Stack.java
+++ Link_Stack.java
@@ -17,8 +17,7 @@ class Stack{
 			System.out.println("null");
 		}
 		
-		node cur = head;
-		
+		node cur = head ;
 		while(cur != null) {
 			System.out.print(cur.value);
 			cur = cur.next;
@@ -31,16 +30,45 @@ class Stack{
 		}
 		
 		node cur = head;
-		while(cur.next == null){
-			cur.next = new node(value);
+		node node_1 = new node(value);
+		
+		while(cur.next != null){
+				cur = cur.next;
+			}
+			cur.next = node_1;
+	}
+
+
+	public void pop() {
+	
+		if (head == null) {
+			System.out.println("no stack");
+			return;
+		}
+		node prev = null;
+		node cur = head;
+		while(cur.next != null){
+				prev = cur; 
+				cur = cur.next;
+			}
+		
+		if(prev == null) {
+			head = head.next;
+		}
+		else{
+		prev.next = null;
 		}
 	}
+
 }
 public class Link_Stack {
 	public static void main(String[] args) {
 		Stack stack = new Stack();
 		stack.push(3);
 		stack.push(4);
+		stack.pop();
+		stack.pop();
+		stack.pop();
 		stack.print_stack();
 	}
 }
